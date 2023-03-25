868bde1baa65af7d38e1128469517e3f0d4d806c
rlgus3351 <rlgus3351@naver.com>
2023-03-26 00:03:49 +0900

    23/03/26/add_comment

--- WWW/src/main/java/com/smhrd/controller/SearchResult.java
+++ WWW/src/main/java/com/smhrd/controller/SearchResult.java
@@ -15,11 +15,19 @@ public class SearchResult extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// 응답 형식 인코딩 지정
 		request.setCharacterEncoding("UTF-8");
+		
+		// 검색 결과 값이 search 파라미터로 전달된다.
 		String search = request.getParameter("search");
 		
+		// 세션 불러오기
 		HttpSession session = request.getSession();
+		
+		// 세션에 값에 search_result의 이름으로 검색받은 결과값 넣어주기
 		session.setAttribute("search_result", search);
+		
+		// 다시 결과창에 보여줘야하므로 search_result.jsp로 이동
 		response.sendRedirect("search_result.jsp");
 		
 	}
--- WWW/src/main/java/com/smhrd/controller/detailContent.java
+++ WWW/src/main/java/com/smhrd/controller/detailContent.java
@@ -16,14 +16,20 @@ public class detailContent extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// 한글 인코딩
 		
+		// 응답받은 값 인코딩
 		request.setCharacterEncoding("UTF-8");
 		
+		// 영화를 클릭하면 데이터에 영화 제목이 data = "영화 제목"으로 오기때문에 data의 값을 불러준다.
 		String data = request.getParameter("data");
 		
+		// 세션 불러오기
 		HttpSession session = request.getSession();
+		
+		// 세션에 data 이름으로 data의 값  넣어주기
 		session.setAttribute("data", data);
+		
+		// 넣은 결과창을 보여줘야 하니까 jsp파일로 이동
 		response.sendRedirect("detailPage.jsp");
 	}
 
